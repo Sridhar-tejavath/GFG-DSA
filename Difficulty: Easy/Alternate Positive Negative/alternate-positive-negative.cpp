44338c3c6da34c960bb9d152ba6ec76a40d5caa4
Tejavath Sridhar <tejavathk.ug23.ec@nitp.ac.in>
2026-08-24 17:32:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    void rearrange(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        vector<int> pos,neg;
+        for(int x:arr){
+            if(x>=0){
+                pos.push_back(x);
+            }
+            else{
+                neg.push_back(x);
+            }
+        }
+        
+        int i=0,j=0,k=0;
+        while(i<pos.size() && j<neg.size()){
+            arr[k++]=pos[i++];
+            arr[k++]=neg[j++];
+        }
+        while(i<pos.size()){
+            arr[k++]=pos[i++];
+        }
+        while(j<neg.size()){
+            arr[k++]=neg[j++];
+        }
+    }
+};
