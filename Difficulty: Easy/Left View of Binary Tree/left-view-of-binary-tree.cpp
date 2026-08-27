8c15f7f2c1428c8167e43f44aeb28d9edbf204f2
Tejavath Sridhar <tejavathk.ug23.ec@nitp.ac.in>
2026-08-27 16:24:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -0,0 +1,44 @@
+/* Structure of Binary Tree Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        // code here
+        vector<int> ans;
+        
+        if(root==NULL) return ans;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+               Node* curr=q.front();
+               q.pop();
+            
+               if(i==0){
+                   ans.push_back(curr->data);
+                }   
+                if(curr->left){
+                   q.push(curr->left);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                 }
+             }
+        }
+        return ans;
+    }
+};
