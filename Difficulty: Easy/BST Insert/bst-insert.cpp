133ce4d6fe6dae6decb5b19c7d96646f109c6378
Tejavath Sridhar <tejavathk.ug23.ec@nitp.ac.in>
2026-09-03 10:30:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BST Insert/bst-insert.cpp
@@ -0,0 +1,43 @@
+/* Structure of a Binary Search Tree node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    Node* insert(Node* root, int key) {
+        // code  here
+        if(root==NULL) return new Node(key);
+        
+        Node *curr=root;
+        while(1){
+            if(key<curr->data){
+                if(curr->left!=NULL){
+                    curr=curr->left;
+                }
+                else{
+                    curr->left=new Node(key);
+                    break;
+                }
+            }
+            else{
+                if(curr->right!=NULL){
+                    curr=curr->right;
+                }
+                else{
+                    curr->right=new Node(key);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+};
