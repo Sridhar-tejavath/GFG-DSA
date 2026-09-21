c0dc8e4daaf6a1dcb958e89b67c6ae2bbaed6220
Tejavath Sridhar <tejavathk.ug23.ec@nitp.ac.in>
2026-09-21 08:29:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum Candy Distribution/minimum-candy-distribution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int minCandy(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        vector<int> candies(n,1);
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[i-1]){
+                candies[i]=candies[i-1]+1;
+            }
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>arr[i+1]){
+                candies[i]=max(candies[i],candies[i+1]+1);
+            }
+        }
+        
+        int ans=0;
+        for(int x:candies){
+            ans += x;
+        }
+        
+        return ans;
+    }
+};
